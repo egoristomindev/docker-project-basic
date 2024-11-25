ae5de490d4b97102a9fc59483222f1101669f31b
Istomin Egor <istomin@zoho.eu>
2024-11-25 17:14:15 +0500

    First commit in docker project

---
+++ python-app/main.py
@@ -0,0 +1,9 @@
+import calendar
+
+print('Добро пожаловать в суперкалендарь\n')
+
+year = int(input('Пожалуйста введите год: '))
+month = int(input('Введите номер любого месяца: '))
+
+print(calendar.month(year, month))
+print('Всего хорошего')
